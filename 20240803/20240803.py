1cd8fa9b8f962c3768940a80ec7000f6f5b4a787
peter <peteraaa13567@gmail.com>
2024-08-03 15:26:40 +0800

    mo

--- 20240803/20240803.py
+++ 20240803/20240803.py
@@ -1,13 +1,14 @@
 from datetime import datetime
-import file
+from tools.file import created_log_file,record_info
+
 
 
 
 def main():
     now = datetime.now()
     current_file_name = now.strftime('%Y_%m_%d.log')
-    log_path = file.created_log_file(current_file_name)
-    file.record_info(log_path)
+    log_path = created_log_file(current_file_name)
+    record_info(log_path)
     
     
 
