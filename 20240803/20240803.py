0d9e669b0f404cbbb7bd3b830a35004e4d6bb9dc
peter <peteraaa13567@gmail.com>
2024-08-03 11:43:02 +0800

    mo

---
+++ 20240803/20240803.py
@@ -0,0 +1,16 @@
+from datetime import datetime
+import file
+
+
+
+def main():
+    now = datetime.now()
+    current_file_name = now.strftime('%Y_%m_%d.log')
+    log_path = file.created_log_file(current_file_name)
+    file.record_info(log_path)
+    
+    
+
+
+if __name__ == '__main__':
+    main()
